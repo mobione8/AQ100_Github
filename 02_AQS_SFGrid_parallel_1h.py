1bf4a68e85cbeb6773fe41018e715db08bdca3f9
mobione8 <mobione8@yahoo.com.sg>
2026-04-10 16:36:53 +0800

    Add files via upload

--- 02_AQS_SFGrid_parallel_1h.py
+++ 02_AQS_SFGrid_parallel_1h.py
@@ -59,14 +59,30 @@ OUTPUT_BASE_DIR = "AQS_SFGridResults"  # NEW output directory for IS/OOS workflo
 #   FILTER_SYMBOLS   = ["AAPL","NVDA"]  → only these symbols
 # ============================================================================
 FILTER_INTERVALS = ["1h"]  # change to None to process all intervals
-FILTER_SYMBOLS = None  # change to ["AAPL","NVDA"] to filter symbols
+# FILTER_SYMBOLS = None  # change to ["AAPL","NVDA"] to filter symbols
 # FILTER_SYMBOLS   = ["FIGR", "CRCL", "DTCX", "USAR", "GLD", "OKLO", "PLTR", "XLC", "ETHA", "MP"]     # change to ["AAPL","NVDA"] to filter symbols
 # FILTER_SYMBOLS   = ["PLTR", "DTCX", "MGNI", "XME", "COPX", "REMX", "HSBC", "RY", "NEE", "PM", "LMT", "KLAC", "GDXJ", "KTOS", "IBKR"]     # Cindy update list 2-29Mar2026
 # FILTER_SYMBOLS   = ["CAT", "JNJ", "NEM", "USO", "XLE", "GS", "FDX", "MS"]
 # FILTER_SYMBOLS   = ["AMZN", "CAT"] completed 1D
 # FILTER_SYMBOLS   = ["PLTR", "DTCX", "MGNI", "XME", "COPX", "REMX", "HSBC", "RY", "NEE", "PM", "LMT", "KLAC", "GDXJ", "KTOS", "IBKR"]     # 1dcompleted # Cindy update list 2-29Mar2026 -
-
 # FILTER_SYMBOLS = ["VDE","XLV","XLP","KO","MCD","WM",]  # new - 7April2026 - next run stage
+# FILTER_SYMBOLS = [
+#     "KLAC",
+#     "CRWD",
+#     "AXON",
+#     "CMI",
+#     "MCK",
+#     "CTRA",
+#     "DLR",
+#     "CEG",
+#     "ARES",
+#     "AIZ",
+#     "DY",
+#     "NET",
+#     "VST",
+#     "OXY",
+# ]  # new - 10April April2026 - commpleted Gridsearch 1h - no results
+FILTER_SYMBOLS = ["FIGR", "AMD", "PANW", "PLTR", "APP", "DDOG"]
 
 # IS/OOS Split Configuration
 IS_OOS_SPLIT = 0.7  # 70% IS, 30% OOS (default)
